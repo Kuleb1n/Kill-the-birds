af0270d3f17051ea6cad1ee2f2d5fb50271888eb
Kuleb1n <v_vpyt@mail.ru>
2022-10-16 18:08:10 +0300

    Bullet class and its movement.

--- main.py
+++ main.py
@@ -26,18 +26,44 @@ class Player(pygame.sprite.Sprite):
     def moving_to_the_left(self):
         self.image = pygame.image.load('images/player_mov_L.png')
 
+    def shoot(self):
+        self.image = pygame.image.load('Images/player.png')
+        bullet = Bullet(self.rect.x, self.rect.y)
+        all_sprites.add(bullet)
+        bullets.add(bullet)
+
+
+class Bullet(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        super().__init__()
+        self.image = pygame.image.load('images/bullet.png')
+        self.image = pygame.transform.scale(self.image, (13, 20))
+        self.rect = self.image.get_rect()
+        self.rect.x = x + 26
+        self.rect.y = y - 8
+        self.speed = 5
+
+    def update(self):
+        self.rect.y -= self.speed
+        if self.rect.y < 0:
+            self.kill()
+
 
 player = Player()
 all_sprites = pygame.sprite.Group()
+bullets = pygame.sprite.Group()
 all_sprites.add(player)
 
 while True:
     clock.tick(FPS)
+    screen.fill(BLACK)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-    screen.fill(BLACK)
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                player.shoot()
 
     key = pygame.key.get_pressed()
     if key[pygame.K_d] and player.rect.x + 100 < WIGHT:
@@ -47,5 +73,6 @@ while True:
         player.moving_to_the_left()
         player.rect.x -= 10
 
+    all_sprites.update()
     all_sprites.draw(screen)
     pygame.display.update()
